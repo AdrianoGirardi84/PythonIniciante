91510e793e9853265b1835a891f8f140a15a7f87
Adriano Girardi <pradrianomarcelo@hotmail.com>
2023-04-20 13:26:27 -0300

    Primeiro commit testing

---
+++ cursodanki.py
@@ -0,0 +1,93 @@
+
+print("Ola mundo")
+
+# -----------------------------------------------------------------
+
+# numeros
+
+# int - inteiro
+# .....-3,-2,-1,0,1,2,3......
+# 1 = int
+
+# float
+# -3, -3.4, -2, 5, 6.3.....
+# 1.0 #float
+
+# complex
+# 1+2j = complex
+
+# ---------------------------------------------------------------------
+
+# variaveis
+
+# A - Z, 0 - 9, e o _ (anderline)
+# altura, Altura, ALTURA
+
+x = 5
+y = 3.5
+z = 1 + 2j
+
+w = "Ana"
+
+(
+    h,
+    j,
+    l,
+) = (
+    1,
+    3,
+    5,
+)
+print(h)
+print(j)
+print(l)
+
+a = b = c = " faz o curso de Python3"
+
+print(a)
+print(b)
+print(c)
+
+h = w + a
+print(h)
+
+# python e dinamico pois podemos reaproveitar
+# a variavel (seu valor pode ser alterado)
+# python é forte porque não permite concatenar texto e números
+
+# --------------------------------------------------------------------------
+
+# STRINGS(TEXTOS)
+
+w = "Curso de Python3"
+print(w)
+
+# METODOS DAS STRINGS
+
+b = " Ola Mundo "
+print(b.strip())
+
+c = "Ola Mundo"
+print(c.lower())
+
+d = "ola mundo"
+print(d.upper())
+
+e = "Ola"
+f = " Mundo"
+g = e + f
+print(g)
+
+# TYPE DE DADOS DAS VARIAVEIS
+
+a = "Ola Mundo"
+b = 5
+c = 3.0
+d = 1 + 2j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print("Testing")
